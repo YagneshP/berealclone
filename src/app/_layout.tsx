dad4502ee4839896f2a9ac79d4d1e2e02f501c0b
yagnesh parekh <yagneshparekh@rocketmail.com>
2026-03-03 22:37:52 -0500

    Add stack screen options

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "yellowgreen" },
+        headerTintColor: "white",
+      }}
+    >
+      <Stack.Screen name='index' options={{ title: "Home" }} />
+      <Stack.Screen name='about' options={{ title: "ABout US" }} />
+    </Stack>
+  );
 }
